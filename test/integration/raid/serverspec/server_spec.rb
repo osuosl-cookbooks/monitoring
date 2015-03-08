b155bf3d1c4fdf6fe5f594cd4250e7c312455f10
Sean Rettig <seanrettig@gmail.com>
2015-03-08 03:55:05 -0700

    fix broken test

--- test/integration/raid/serverspec/server_spec.rb
+++ test/integration/raid/serverspec/server_spec.rb
@@ -3,8 +3,9 @@ require 'serverspec'
 set :backend, :exec
 
 describe file('/usr/lib64/nagios/plugins/check_hpacucli') do
-  it { should exist }
+  it { should be_file }
+end
 
-describe file('/etc/nagios/nrpe.d/check_http.cfg') do
+describe file('/etc/nagios/nrpe.d/check_hpacucli.cfg') do
   its(:content) { should match /command\[check_hpacucli\]=\/usr\/lib64\/nagios\/plugins\/check_hpacucli -t/ }
 end
