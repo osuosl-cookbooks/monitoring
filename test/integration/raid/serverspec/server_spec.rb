cf4524c07a2747a20c1adcdd683e511808831388
Sean Rettig <seanrettig@gmail.com>
2015-03-25 01:55:04 -0700

    update tests for new nrpe name format

--- test/integration/raid/serverspec/server_spec.rb
+++ test/integration/raid/serverspec/server_spec.rb
@@ -6,6 +6,6 @@ describe file('/usr/lib64/nagios/plugins/check_hpacucli') do
   it { should be_file }
 end
 
-describe file('/etc/nagios/nrpe.d/check_hpacucli.cfg') do
-  its(:content) { should match %r{command\[check_hpacucli\]=\/usr\/lib64\/nagios\/plugins\/check_hpacucli -t} }
+describe file('/etc/nagios/nrpe.d/check_raid_hp.cfg') do
+  its(:content) { should match %r{command\[check_raid_hp\]=\/usr\/lib64\/nagios\/plugins\/check_hpacucli -t} }
 end
