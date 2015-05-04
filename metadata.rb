3b16f620a776423f170ee0c9691ddb6eb6796d5c
Sean Rettig <seanrettig@gmail.com>
2015-05-04 13:23:40 -0700

    start on nagios 7 upgrade, change versions

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'monitoring'
 maintainer       'Oregon State University'
-maintainer_email 'rudy@grigar.net'
+maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures monitoring'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.9'
+version          '1.0.0'
 
 depends          'munin'
 depends          'nagios'
