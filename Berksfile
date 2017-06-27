6d04c8817118d39d073e67a055c1323e7c54fd14
Lance Albertson <lance@osuosl.org>
2017-06-27 09:19:09 -0700

    Deprecating cookbooks and removing files

--- Berksfile
+++
@@ -1,7 +0,0 @@
-source 'https://supermarket.getchef.com'
-
-cookbook "munin"
-cookbook "nagios", git: "github.com:osuosl-cookbooks/nagios"
-cookbook "osl-munin", git: "git@github.com:osuosl-cookbooks/osl-munin"
-
-metadata
--- test/integration/smtp/serverspec/server_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'serverspec'
-
-set :backend, :exec
-
-describe file('/etc/nagios/nrpe.d/check_mailq.cfg') do
-  it { should contain('command[check_mailq]=/usr/lib64/nagios/plugins/check_mailq -w 5000 -c 10000 -M postfix') }
-end
